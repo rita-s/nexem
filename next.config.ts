37a06dc1dea273985178ac8b3e922c53458a2bf6
Rita Starzyk <rita_s@mail.com>
2025-02-02 02:04:07 +0000

    Updated config files for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    basePath: '/nexem',
+    images: {
+        unoptimized: true,
+    },
+    // This is important for GitHub Pages
+    assetPrefix: '/nexem/',
+    // Disable server-side features
+    trailingSlash: true,
+    // Compiler options
+    compiler: {
+        // Remove console.logs in production
+        removeConsole: process.env.NODE_ENV === "production",
+    }
 };
 
 export default nextConfig;
